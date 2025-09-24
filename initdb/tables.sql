97fd0b58c92b624a218ae4b5fa811f38d70ca59c
albert <shoma.albert081205@gmail.com>
2025-09-24 23:10:33 +0300

    create table students

--- initdb/tables.sql
+++ initdb/tables.sql
@@ -1,8 +1,13 @@
 CREATE TABLE users (
                        id serial primary key,
-                       book_id int unique not null,
-                       surname text not null,
-                       name text not null,
-                       middle_name text,
-                       birth_date date
+                       book_id int unique not null
+);
+CREATE TABLE students (
+                          id serial primary key,
+                          book_id int unique not null,
+                          surname text not null,
+                          name text not null,
+                          middle_name text not null,
+                          birth_date date,
+                          student_group text not null
 )
